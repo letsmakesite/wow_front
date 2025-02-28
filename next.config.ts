453de7e3babd30bec6fcad25236bbe2ad6a03dc2
Viacheslav <viacheslav.webdesign@gmail.com>
2025-02-28 15:13:59 +0200

    next config fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  output: "export",
   i18n: {
     locales: ["uk", "en"],
     defaultLocale: "uk",
